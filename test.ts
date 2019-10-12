8497c779c6234c3e30bcaf9d956b59cb5cf9be3f
Stefan Djordjevic <stefandjordjevic@live.com>
2019-10-12 20:51:18 +0200

    cleanup

--- test.ts
+++ test.ts
@@ -1,14 +1,1 @@
-const task = () => {
-  return new Promise((resolve, reject) => {
-    const b = 1;
-    setTimeout(()=> 
-    {console.log('Hey'); resolve()},3000);
-    console.log(b);
-  });
-}
 
-async function init(){
-  await task();
-}
-
-init();
